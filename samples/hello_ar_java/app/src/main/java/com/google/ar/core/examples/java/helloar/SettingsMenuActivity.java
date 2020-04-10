ca0a3ac2ab5f79cc2d9c749df9ebd8e29bc6ea35
spazking55 <47401483+spazking55@users.noreply.github.com>
2020-04-10 15:35:15 -0400

    Settings Activity

---
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/SettingsMenuActivity.java
@@ -0,0 +1,15 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsMenuActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_menu);
+
+    }
+}
