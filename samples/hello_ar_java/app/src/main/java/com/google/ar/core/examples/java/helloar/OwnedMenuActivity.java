dc2f2e59c6977ccaafe1f2571f2090f412efb2a6
JacobCGibson <62563324+JacobCGibson@users.noreply.github.com>
2020-04-10 14:32:42 -0400

    Add files via upload

---
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/OwnedMenuActivity.java
@@ -0,0 +1,14 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OwnedMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owned_menu);
+
+    }
+}
