cd73129d9fc2290fec9e035684cf34379155ac04
seastarr2799 <62966414+seastarr2799@users.noreply.github.com>
2020-04-23 01:17:18 -0400

    Create ScoreboardAttr.java

---
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/ScoreboardAttr.java
@@ -0,0 +1,14 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.provider.BaseColumns;
+
+public final class ScoreboardAttr {
+    public ScoreboardAttr() {}
+
+    public static abstract class ScoreboardEntry implements BaseColumns {
+        public static final String TABLE_NAME = "player";
+        public static final String COLUMN_INITIALS = "initials";
+        public static final String COLUMN_SCORE = "score";
+        public static final String COLUMN_TIME = "time";
+    }
+}
